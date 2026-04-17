a6f06914da137108c15c8c07abd9804563185769
Maximilian Güntner <code@mguentner.de>
2026-04-17 23:39:03 +0200

    chore: update flake and nixpkgs

--- flake.nix
+++ flake.nix
@@ -3,39 +3,59 @@
   inputs.nix-github-actions.url = "github:nix-community/nix-github-actions";
   inputs.nix-github-actions.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = { self, nixpkgs, nix-github-actions, ... }: let
-    overlay = import ./nix/overlay.nix;
-
-    supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
-
-    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-
-    nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ overlay ]; });
-  in {
-    overlays.default = overlay;
-
-    packages = forAllSystems (system:
+  outputs =
     {
-      inherit (nixpkgsFor.${system}) cannelloni;
-    });
+      self,
+      nixpkgs,
+      nix-github-actions,
+      ...
+    }:
+    let
+      overlay = import ./nix/overlay.nix;
 
-    defaultPackage = forAllSystems (system: (nixpkgsFor.${system}).cannelloni);
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
 
-    devShells = forAllSystems (system:
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      nixpkgsFor = forAllSystems (
+        system:
+        import nixpkgs {
+          inherit system;
+          overlays = [ overlay ];
+        }
+      );
+    in
     {
-      default = import ./shell.nix { pkgs = (nixpkgsFor.${system}); };
-    });
+      overlays.default = overlay;
 
-    nixosModules.default = import ./nix/module.nix;
+      packages = forAllSystems (
+        system:
+        let
+          cannelloni = (nixpkgsFor.${system}).cannelloni;
+        in
+        {
+          inherit cannelloni;
+          default = cannelloni;
+        }
+      );
 
-    checks = forAllSystems (system: {
-      sctp = nixpkgsFor.${system}.callPackage ./nix/tests/sctp.nix {};
-      tcp = nixpkgsFor.${system}.callPackage ./nix/tests/tcp.nix {};
-      udp = nixpkgsFor.${system}.callPackage ./nix/tests/udp.nix {};
-    });
+      devShells = forAllSystems (system: {
+        default = import ./shell.nix { pkgs = (nixpkgsFor.${system}); };
+      });
 
-    githubActions = nix-github-actions.lib.mkGithubMatrix {
-      checks = nixpkgs.lib.getAttrs [ "x86_64-linux" ] self.checks;
+      nixosModules.default = import ./nix/module.nix;
+
+      checks = forAllSystems (system: {
+        sctp = nixpkgsFor.${system}.callPackage ./nix/tests/sctp.nix { };
+        tcp = nixpkgsFor.${system}.callPackage ./nix/tests/tcp.nix { };
+        udp = nixpkgsFor.${system}.callPackage ./nix/tests/udp.nix { };
+      });
+
+      githubActions = nix-github-actions.lib.mkGithubMatrix {
+        checks = nixpkgs.lib.getAttrs [ "x86_64-linux" ] self.checks;
+      };
     };
-  };
 }
